5cdd9cb3fe073260b8e008c85d21259c8640f8c7
addankisripallavi3-hue <addankisripallavi3@gmail.com>
2026-06-19 15:38:20 +0530

    Create Buspass.java

---
+++ Buspass.java
@@ -0,0 +1,17 @@
+public class BusPass {
+	    String name;
+	    int age;
+	    String source;
+	    String destination;
+	    double price;
+
+	    public BusPass(String name, int age, String source,
+	                   String destination, double price) {
+
+	        this.name = name;
+	        this.age = age;
+	        this.source = source;
+	        this.destination = destination;
+	        this.price = price;
+	    }
+	}
