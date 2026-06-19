13280cfb2385f3005415ef51bf61a212a4dad1a8
addankisripallavi3-hue <addankisripallavi3@gmail.com>
2026-06-19 15:38:56 +0530

    Create TicketPrice.java

---
+++ TicketPrice.java
@@ -0,0 +1,8 @@
+public class TicketPrice {
+
+	    public static double calculatePrice(String source,
+	                                        String destination) {
+
+	        return 150.0;
+	    }
+	}
